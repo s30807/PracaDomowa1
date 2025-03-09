1acced7d1981c8a93e0e9f9feb79f1bb543024aa
s30807 <s30807@pjwstk.edu.pl>
2025-03-09 22:38:41 +0100

    Feature average

---
+++ AplikacjaSrednia2/AplikacjaSrednia2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program{
+static void Main()
+{
+    int[] numbers = { 1, 2, 3, 4, 5 };
+    Console.WriteLine("Średnia: "+CalculateAverage(numbers));
+}
+static double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+    {
+        Console.WriteLine("Tablica nie moze byc pusta");
+    }
+
+    return numbers.Average();
+}
+}
